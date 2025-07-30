0ac2d0563da38e8f9a98f3e62f98edb4956f79db
Andrei Vilnoiu <Vilnoiuandrei@gmail.com>
2025-07-30 23:58:13 +0300

    Fix a comma

--- app/api/getUserId/route.js
+++ app/api/getUserId/route.js
@@ -6,18 +6,15 @@ export async function GET() {
   const session = await auth();
 
   if (!session) {
-    return (
-      Response.json({ error: "Unauthorized" }),
-      {
-        status: 401,
-      }
-    );
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   const client = await clientPromise;
   const db = client.db("test");
   const email = session.user.email;
   const user = await db.collection("users").findOne({ email });
-  if (!user)
+
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
   return NextResponse.json({ userId: user._id.toString() }, { status: 200 });
 }
